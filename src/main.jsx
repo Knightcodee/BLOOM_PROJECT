c5bcf35158c9d2a5e2add35900c7a634b4855b82
Knightcodee <kanaha.sahoo2016@gmail.com>
2024-08-07 15:23:15 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/BLOOM-PROJECT">
+    <BrowserRouter basename={window.location.pathname || '' }>>
       <ChakraProvider>
         <App />
       </ChakraProvider>
