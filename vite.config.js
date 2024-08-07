ff59c81f7488e943b1b375cb082d7adecd9dbc89
Knightcodee <kanaha.sahoo2016@gmail.com>
2024-08-07 14:02:53 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/BLOOM-PROJECT/"
-
-})
+  base: '/BLOOM-PROJECT/', // Make sure this matches your GitHub Pages repository name
+});
